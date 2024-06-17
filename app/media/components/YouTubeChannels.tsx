5f3d9084b5620fc7aecff62e04255fcfdf172784
willd <will.donnell652@gmail.com>
2024-06-17 19:00:03 +0100

    Potential Axios fix

--- app/media/components/YouTubeChannels.tsx
+++ app/media/components/YouTubeChannels.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios from 'axios'
 import styles from './YouTubeChannels.module.css';
 import { LinkPreview } from '@/app/components/link-preview';
 
