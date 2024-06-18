ac0a24e35b5b225d4a94c77078956e49dfe1ab2d
willd <will.donnell652@gmail.com>
2024-06-18 15:51:45 +0100

    more minor tweaks and updated readme

--- app/components/InfoPill.tsx
+++ app/components/InfoPill.tsx
@@ -2,12 +2,18 @@ import styles from "./InfoPill.module.css"
 import { LinkPreview } from "./link-preview"
 
 export default function infoPill(){
+
+    const currentDate = new Date()
+
+    const extraYear = ((currentDate.getDate() > 11) || (currentDate.getMonth() > 0)) ? 0:-1
+    const age = (currentDate.getFullYear()-2005) + extraYear
+
     return(
     <div className={styles.main}>
         <h3 className={styles.main_h3}>🚀 Founder at <span className="Link"><LinkPreview url="https://induct.ie" className="Link">Induct</LinkPreview></span></h3>
         <h3 className={styles.main_h3}>🛠️ Engineer & Developer</h3>
         <h3 className={styles.main_h3}>🎓 Trinity College Dublin</h3>
-        <h3 className={styles.main_h3}>⏰ 19 years old</h3>
+        <h3 className={styles.main_h3}>⏰ {age} years old</h3>
         <h3 className={styles.main_h3}>☘️ Irish citizen</h3>
         <h3 className={styles.main_h3}>🏡 Dublin city</h3>
     </div>
